0bbd1a002d25d1169369180af619348e70316e9a
Florian Dutronc <>
2022-03-20 23:48:16 +0100

    [bug] fix error

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(require('./controllers'));
 // Connect database
 db.connect(() => {
   // Listen for connection
-  app.listen(process.env.PORT || 5000), () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log('Server listening to port 3000');
   });
 });
